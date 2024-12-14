bc54ab6f8758b43c6bedf489b9c08174d4907272
Giovanna Rosace <rosacegiovanna@gmail.com>
2024-12-14 11:10:59 +0100

    Aggiornamento Coremap

--- os161-base-2.0.3/kern/include/coremap.h
+++ os161-base-2.0.3/kern/include/coremap.h
@@ -7,10 +7,18 @@
  * Structure used to keep track of the state of all memory pages.
  */
 struct coremap_entry {
-    unsigned bool occupied;       // Defines the state of the page 1= occupied  0=free
-    //altri campi
+    bool occupied;       // Defines the state of the page 1=occupied  0=free
+    //per il kernerl
+    unsigned long allocSize; //quante pagine a partire dalla pagina corrente sono allocate
+    unsigned long prevAllocated;
+    unsigned long nextAllocated;
+    //in più per l'user
+    struct addrspace *as; //addrespace della pagina richiesta
+    vaddr_t vaddr; //indirizzo d'inizio della pagina richiesta
 };
 
+void coremap_init(void);
+void coremap_shutdown(void);
 vaddr_t alloc_kpages(unsigned npages);
 void free_kpages(vaddr_t addr);
 
